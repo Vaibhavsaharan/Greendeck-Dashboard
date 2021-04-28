7a659462f63f34c645a9d05e0c974ac155eb400a
Vaibhav Saharan <vaibhavsaharans@gmail.com>
2021-04-28 23:34:48 +0530

    added dashboardui

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors())
 app.use(router)
 connectDB();
-app.use(express.static('client/build'));
+app.use(express.static('dashboardui/build'));
 
 
 const metricsmodel = mongoose.model('datafiles', schema);
